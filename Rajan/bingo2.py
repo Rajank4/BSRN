3bc118f15a43b7f7ea475213c87e449bd6f8a437
rajan <rajan.kakkar@stud.fra-uas.de>
2024-06-30 05:14:30 +0200

    PyTermTk Bingofeld Größenverstellbar

--- Rajan/bingo2.py
+++ Rajan/bingo2.py
@@ -1,200 +1,174 @@
-import random
-from textual import on
-from textual.app import App, ComposeResult
-from textual.widgets import Label, Footer, Button, Static, Header
-import sys
 import os
-import time
-
-print(100 * "\n" + "<......Bingo Game......>")
-
-buzzword_Wörter = []
-
-while True:
-    spielname = input("Bitte geben Sie den Spielnamen ein, mit dem Sie sich verbinden wollen: ")
-    spielname = spielname.replace("/", "").replace("\\", "")
-    if os.path.exists(f"{spielname}.txt"):
-        eingabe = input(
-            f"Das Spiel {spielname} läuft bereits.\nSie werden dem Spiel {spielname} beitreten.\n\nBestätigen Sie mit Enter.\n'exit' wird den Prozess beenden. Jede andere Eingabe lässt Sie einen neuen Spielnamen eingeben.")
-        if eingabe == "":
-            with open(f"{spielname}.txt", 'r') as file:
-                size = int(file.readline().strip())
-                buzzword_Wörter = [zeile.strip() for zeile in file]
-            break
-        elif eingabe == "exit":
-            sys.exit()
-        else:
-            continue
-    else:
-        eingabe = input(
-            f"Das Spiel {spielname} existiert noch nicht.\nSie werden das Spiel {spielname} erstellen und hosten.\n\nBestätigen Sie mit Enter.\n'exit' wird den Prozess beenden. Jede andere Eingabe lässt Sie einen neuen Spielnamen eingeben.")
-        if eingabe == "":
-            while True:
-                try:
-                    size = int(input("Bitte gebe die Spielfeldgröße ein: "))
-                    if size < 3 or size > 7:
-                        print("Ungültige Größe: Erlaubt ist eine Größe von 3 - 7 und sie darf keine Dezimalzahl sein")
-                    else:
-                        break
-                except ValueError:
-                    continue
-            while True:
-                try:
-                    eingabe = input(
-                        "Möchtest du die standard Buzzwords-Datei verwenden, drücke Enter. Sonst gib den Pfad an.\nDiese Funktion steht nur zur Verfügung, wenn das Spiel direkt aus dem Ordner gestartet wird.")
-                    if eingabe == "":
-                        dateipfad = "buzzwords.txt"
-                    else:
-                        dateipfad = eingabe
-                    buzzword_Wörter = []
-                    with open(dateipfad, 'r') as file:
-                        buzzword_Wörter = [zeile.strip() for zeile in file]
-                    if len(buzzword_Wörter) < size * size:
-                        print("Die Anzahl der Wörter in der Datei ist zu gering!")
-                        continue
-                    with open(f"{spielname}.txt", 'w') as file:
-                        file.write(f"{size}\n")
-                        for wort in buzzword_Wörter:
-                            file.write(f"{wort}\n")
-                    break
-                except Exception:
-                    print("Der Dateipfad ist ungültig!")
-            break
-        elif eingabe == "exit":
-            sys.exit()
-        else:
-            continue
+import sys
+import logging
+import random
+from datetime import datetime
+import TermTk as ttk
+from TermTk.TTkCore.color import TTkColor
+from TermTk.TTkWidgets.button import TTkButton
+from TermTk.TTkWidgets.label import TTkLabel
+from TermTk.TTkWidgets.window import TTkWindow
+from TermTk.TTkWidgets.frame import TTkFrame
+from TermTk.TTkLayouts.gridlayout import TTkGridLayout
 
 
-class Bingo(App):
-    CSS_PATH = "styles.tcss"
-    spielname = spielname
-    size = size
-    buzzword_Wörter = buzzword_Wörter
-    button_status = {}
-    SiegerProzess = False
-    SpielVorbei = False
+class CustomTTkButton(TTkButton):
+    def setBgColor(self, color):
+        self.style()['default']['bg'] = TTkColor.fg(color) if color else None
+        self.update()
 
-    def bingo_felder_erstellen(self, size):
-        self.grid_container.remove_children()  # Entfernt das alte Grid
 
-        self.button_name_Liste = []  # Liste zum Speichern der Wörter
+class StartPage:
+    def __init__(self, root):
+        self.root = root
+        self.window = TTkWindow(parent=self.root, pos=(1, 1), size=(70, 30), title="Buzzword Bingo :)", layout=TTkGridLayout())
 
-        benutzte_wörter = []
-        for y in range(size):
-            for x in range(size):
-                if size in {5, 7} and y == size // 2 and x == size // 2:
-                    button_name = "Joker Feld"
-                else:
-                    while True:
-                        button_name = random.choice(self.buzzword_Wörter)
-                        if button_name not in benutzte_wörter:
-                            benutzte_wörter.append(button_name)
-                            break
-                self.button_name_Liste.append(button_name)
-                button_id = f"button_{y}_{x}"
-                self.button_status[button_id] = False  # Initialer Status des Buttons ist False (nicht durchgestrichen)
-                if size in {5, 7} and y == size // 2 and x == size // 2:
-                    self.button_status[button_id] = True  # Joker Feld ist automatisch durchgestrichen
-                    button = Button(button_name, id=button_id, classes="Joker")
-                    button.disabled = True
-                else:
-                    button = Button(button_name, id=button_id, classes="bingo-button")
-                self.grid_container.mount(button)
+        # Welcome Label
+        welcome_frame = TTkFrame(parent=self.window, border=False, layout=TTkGridLayout())
+        welcome_label = TTkLabel(parent=welcome_frame, text="Willkommen zum Buzzword Bingo!", alignment=ttk.TTkK.CENTER)
+        welcome_frame.layout().addWidget(welcome_label, 0, 0)
 
-        self.grid_container.styles.grid_size_columns = size  # Formatiert in CSS
-        self.grid_container.styles.grid_size_rows = size
+        # Buttons
+        button_frame = TTkFrame(parent=self.window, border=False, layout=TTkGridLayout(), size=(70, 5))
+        start_button = CustomTTkButton(border=True, text="Spiel starten", checkable=True)
+        start_button.clicked.connect(self.start_game)
+        button_frame.layout().addWidget(start_button, 0, 0)
 
-    def überprüfe_bingo(self):
-        # Horizontale Überprüfung
-        for y in range(self.size):
-            if all(self.button_status.get(f"button_{y}_{x}", False) for x in range(self.size)):
-                self.bingo_confirm_button.add_class("bingoconf")  # Fügt die "bingoconf"-Klasse zum Button hinzu
+        close_button = CustomTTkButton(border=True, text="Spiel beenden", checkable=True)
+        close_button.clicked.connect(self.spiel_beenden)
+        button_frame.layout().addWidget(close_button, 0, 1)
+
+        self.window.layout().addWidget(welcome_frame, 0, 0, colspan=2)
+        self.window.layout().addWidget(button_frame, 1, 0, colspan=2)
+
+    def start_game(self):
+        self.window.close()
+        if len(sys.argv) != 6:
+            print("Bitte geben Sie die notwendigen Argumente ein: [Spielername] [Pfad zur Buzzwords-Datei] [Log-Datei Speicherort] [Anzahl Zeilen] [Anzahl Spalten]")
+            sys.exit(1)
+
+        spielername = sys.argv[1]
+        roundfile = sys.argv[2]
+        log_path = sys.argv[3]
+        zeilen = int(sys.argv[4])
+        spalten = int(sys.argv[5])
+
+        GamePage(self.root, spielername, roundfile, log_path, zeilen, spalten)
+
+    def spiel_beenden(self):
+        now = datetime.now()
+        logging.info(f"{now.strftime('%Y-%m-%d %H:%M:%S')} - Abbruch des Spiels von der Startseite aus")
+        logging.info(f"{now.strftime('%Y-%m-%d %H:%M:%S')} - Ende des Spiels von der Startseite aus")
+        sys.exit(0)
+
+
+class GamePage:
+    def __init__(self, root, spielername, roundfile, log_path, zeilen, spalten):
+        self.spielername = spielername
+        self.roundfile = roundfile
+        self.log_path = log_path
+        self.zeilen = zeilen
+        self.spalten = spalten
+
+        self.buzzwords = self.read_buzzword(self.roundfile)
+        self.used_buzzwords = set()
+
+        self.create_log_file()
+
+        self.root = root
+        window_width = spalten * 12 + 10  # Adjust width based on columns
+        window_height = zeilen * 3 + 10   # Adjust height based on rows
+        self.window = TTkWindow(parent=self.root, pos=(1, 1), size=(window_width, window_height), title="Buzzword Bingo :)", layout=TTkGridLayout())
+
+        self.buttons = [[None for _ in range(self.spalten)] for _ in range(self.zeilen)]
+
+        self.setup_game()
+
+    def create_log_file(self):
+        os.makedirs(self.log_path, exist_ok=True)
+        now = datetime.now()
+        date_string = now.strftime("%Y-%m-%d")
+        log_file_name = os.path.join(self.log_path, f"log-{date_string}-{self.spielername}.txt")
+        logging.basicConfig(filename=log_file_name, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+        logging.info(f"Log-Datei für {self.spielername} erstellt.")
+        logging.info(f"Größe des Spielfelds Zeilen: {self.zeilen}, Spalten: {self.spalten}")
+
+    def read_buzzword(self, roundfile):
+        with open(roundfile, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+            return lines
+
+    def spiel_beenden(self):
+        now = datetime.now()
+        logging.info(f"{now.strftime('%Y-%m-%d %H:%M:%S')} - Abbruch des Spiels von der Spielseite aus")
+        logging.info(f"{now.strftime('%Y-%m-%d %H:%M:%S')} - Ende des Spiels von der Spielseite aus")
+        sys.exit(0)
+
+    def check_win(self):
+        for i in range(self.zeilen):
+            if all(self.buttons[i][j].isChecked() for j in range(self.spalten)):
                 return True
-
-        # Vertikale Überprüfung
-        for x in range(self.size):
-            if all(self.button_status.get(f"button_{y}_{x}", False) for y in range(self.size)):
-                self.bingo_confirm_button.add_class("bingoconf")  # Fügt die "bingoconf"-Klasse zum Button hinzu
+        for j in range(self.spalten):
+            if all(self.buttons[i][j].isChecked() for i in range(self.zeilen)):
                 return True
-
-        # Diagonale Überprüfung (von links oben nach rechts unten)
-        if all(self.button_status.get(f"button_{i}_{i}", False) for i in range(self.size)):
-            self.bingo_confirm_button.add_class("bingoconf")  # Fügt die "bingoconf"-Klasse zum Button hinzu
+        if all(self.buttons[i][i].isChecked() for i in range(min(self.zeilen, self.spalten))):
             return True
-
-        # Diagonale Überprüfung (von rechts oben nach links unten)
-        if all(self.button_status.get(f"button_{i}_{self.size - 1 - i}", False) for i in range(self.size)):
-            self.bingo_confirm_button.add_class("bingoconf")  # Fügt die "bingoconf"-Klasse zum Button hinzu
+        if all(self.buttons[i][self.spalten - 1 - i].isChecked() for i in range(min(self.zeilen, self.spalten))):
             return True
-
-        self.bingo_confirm_button.remove_class("bingoconf")  # Entfernt die "bingoconf"-Klasse vom Button
         return False
 
-    def compose(self):
-        self.zufallswort_label = Label("", id="zufallswort_label")
-        self.error_message = Label("", id="error_message")
-        self.zufallswort_button = Button("Wort generieren", id="zufallswort")
-        self.grid_container = Static(classes="bingo-grid")
-        self.gewonnen_label = Label("", id="gewonnen_label")
-        self.bingo_confirm_button = Button("Bingo bestätigen", id="bingo_confirm")
-        self.quit_button = Button("Quit", id="quit", classes="quit-button")
-        self.getWort = Label("", id="getWort")
-        self.wortlisteTitel = Label("\nListe bisheriger Wörter:\n", id="wortlisteTitel", classes="wortlisteTitel")
-        self.wortliste_label = Label("", id="wortliste_label")
-        self.CheckBingo_label = Label("", id="CheckBingo_label")
-        self.title = f"Spielname: {self.spielname} \t" + f"\tSpielerID: {os.getpid()}"
-
-        yield Header("", id="header-ID", classes="header")
-        yield self.error_message
-        yield self.zufallswort_label
-        yield self.zufallswort_button
-        yield self.grid_container
-        yield self.CheckBingo_label
-        yield self.gewonnen_label
-        yield self.bingo_confirm_button
-        yield self.quit_button
-        yield self.wortlisteTitel
-        yield self.wortliste_label
-        yield Footer()
-
-    def on_mount(self):
-        self.bingo_felder_erstellen(self.size)
-
-    @on(Button.Pressed)
-    def wort_streiche(self, event: Button.Pressed):
-        buttonName = str(event.button.label)
-        button_id = str(event.button.id)
-        if button_id.startswith("button_"):
-            if button_id in self.button_status:
-                self.button_status[button_id] = not self.button_status[button_id]
-                if self.button_status[button_id]:
-                    event.button.add_class("strikethrough")
-                else:
-                    event.button.remove_class("strikethrough")
-                if self.überprüfe_bingo():
-                    self.update_gewonnen_label("")
-
-    @on(Button.Pressed, "#zufallswort")
-    def zufallswort_generieren(self):
-        zufallswort = random.choice(self.buzzword_Wörter)
-        self.zufallswort_label.update(zufallswort)
-
-    @on(Button.Pressed, "#bingo_confirm")
-    def on_bingo_confirm(self, event):
-        if self.überprüfe_bingo():
-            self.update_gewonnen_label("Bingo!")
-            self.SiegerProzess = True
+    def log_buzzword(self, button, row, col):
+        now = datetime.now()
+        button_text = button.text()
+        if button.isChecked():
+            logging.info(f"{now.strftime('%Y-%m-%d %H:%M:%S')} - Button geklickt: {button_text}  (Zeile: {row + 1}, Spalte: {col + 1})")
+            button.setBgColor('#88ffff')
         else:
-            self.update_gewonnen_label("Noch kein Bingo. Versuche es weiter!")
+            logging.info(f"{now.strftime('%Y-%m-%d %H:%M:%S')} - Button rückgängig: {button_text} (Zeile: {row + 1}, Spalte: {col + 1})")
+            button.setBgColor(None)
+        if self.check_win():
+            self.show_winner()
 
-    @on(Button.Pressed, "#quit")
-    def quit_game(self):
-        self.exit("Quit")
+    def show_winner(self):
+        winner_window = TTkWindow(parent=self.root, pos=(10, 10), size=(50, 10), title="Gewinner!", layout=TTkGridLayout())
+        winner_label = TTkLabel(parent=winner_window, text=f"Herzlichen Glückwunsch, {self.spielername}!", alignment=ttk.TTkK.CENTER)
+        winner_window.layout().addWidget(winner_label, 0, 0, colspan=2)
 
-    def update_gewonnen_label(self, text):
-        self.gewonnen_label.update(text)
+        close_button = CustomTTkButton(border=True, text="Spiel beenden", checkable=True)
+        close_button.clicked.connect(self.spiel_beenden)
+        winner_window.layout().addWidget(close_button, 1, 0, colspan=2)
+
+        self.root.addWidget(winner_window)
+        winner_window.raiseWidget()
+
+    def setup_game(self):
+        for i in range(self.zeilen):
+            for j in range(self.spalten):
+                if self.zeilen % 2 != 0 and self.spalten % 2 != 0 and i == self.zeilen // 2 and j == self.spalten // 2:
+                    btn = CustomTTkButton(border=True, text="Joker", checkable=True)
+                    btn.setBgColor(color='#ff88ff')
+                    self.window.layout().addWidget(btn, i, j)
+                    self.buttons[i][j] = btn
+                else:
+                    buzzword = random.choice(self.buzzwords).strip()
+                    while buzzword in self.used_buzzwords:
+                        buzzword = random.choice(self.buzzwords).strip()
+                    self.used_buzzwords.add(buzzword)
+                    btn = CustomTTkButton(border=True, text=buzzword, checkable=True)
+                    btn.clicked.connect(lambda b=btn, row=i, col=j: self.log_buzzword(b, row, col))
+                    self.window.layout().addWidget(btn, i, j)
+                    self.buttons[i][j] = btn
+
+        close_button = CustomTTkButton(border=True, text="Spiel beenden", checkable=True)
+        close_button.clicked.connect(self.spiel_beenden)
+        self.window.layout().addWidget(close_button, self.zeilen, self.spalten - 1)
+
+
+def main():
+    root = ttk.TTk()
+    StartPage(root)
+    root.mainloop()
 
 
 if __name__ == "__main__":
-    Bingo().run()
+    main()
